73b3e188b5065c076ae0a3189b74c3532c9492e8
choco(jm) <jmchoco.0501@gmail.com>
2021-03-27 02:14:09 +0900

    1. 앱 아이콘 추가 2. 앱 심사중

--- StockCalculator/ViewControllerFiles/ViewController.swift
+++ StockCalculator/ViewControllerFiles/ViewController.swift
@@ -47,9 +47,28 @@ class ViewController: UIViewController {
         firstLoad?()
     }
     
+//    override func viewDidAppear(_ animated: Bool) {
+//        checkDeviceNetworkStatus()
+//    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         pagingViewStart()
         adMobStart()
     }
+    
+//    func checkDeviceNetworkStatus() {
+//            if(DeviceManager.shared.networkStatus) {
+//                let firstVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController")
+//                present(firstVC, animated: true, completion: nil)
+//
+//            } else {
+//                let alert: UIAlertController = UIAlertController(title: "네트워크 상태 확인", message: "네트워크가 불안정 합니다.", preferredStyle: .alert)
+//                let action: UIAlertAction = UIAlertAction(title: "다시 시도", style: .default, handler: { (ACTION) in
+//                    self.checkDeviceNetworkStatus()
+//                })
+//                alert.addAction(action)
+//                present(alert, animated: true, completion: nil)
+//            }
+//        }
 }
